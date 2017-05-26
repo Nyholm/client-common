1e50372b21c16f97c7ffad66112929ff3cace1dc
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-05-26 21:08:06 +0200

    Remove need for discovery

--- src/PluginClientFactory.php
+++ src/PluginClientFactory.php
@@ -3,15 +3,34 @@
 namespace Http\Client\Common;
 
 /**
+ * A configurable PluginClientFactory. If no call to setFactory has been made we will fallback on the default PluginFactory.
+ *
  * @author Fabien Bourigault <bourigaultfabien@gmail.com>
  */
 final class PluginClientFactory implements PluginClientFactoryInterface
 {
     /**
+     * @var PluginClientFactoryInterface|null
+     */
+    private static $factory;
+
+    /**
+     * @param PluginClientFactoryInterface $factory
+     */
+    public static function setFactory(PluginClientFactoryInterface $factory = null)
+    {
+        self::$factory = $factory;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function createClient($client, array $plugins = [], array $options = [])
     {
+        if (null !== self::$factory) {
+            return self::$factory->createClient($client, $plugins, $options);
+        }
+
         return new PluginClient($client, $plugins, $options);
     }
 }
